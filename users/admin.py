64bf1b9a7d362e93e0cf369c7f4bcaad904f12a6
Adeoluwa <oliyideadeoluwajohn1@gmail.com>
2024-02-06 12:51:07 +0100

    New Upgrade

--- users/admin.py
+++ users/admin.py
@@ -1,9 +1,4 @@
 from django.contrib import admin
 from .models import UserAccount
 
-
-class UserAdmin(admin.ModelAdmin):
-    search_fields  = ['full_name', 'username', 'email',  'phone']
-    list_display  = ['full_name', 'username', 'email',  'phone']
-
-admin.site.register(UserAccount, UserAdmin)
+admin.site.register(UserAccount)
